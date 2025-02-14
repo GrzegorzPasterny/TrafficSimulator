ecfb1efe61f76062e2c3705f2efc77b780658f1a
Grzegorz Pasterny <grzegorz.pasterny95@gmail.com>
2025-02-14 21:47:02 +0100

    Mske tes work hut no  exretoosi

--- test/TrafficSimulator.Application.UnitTests/SimulationHandlerTests/SimulationHandlerTests.cs
+++ test/TrafficSimulator.Application.UnitTests/SimulationHandlerTests/SimulationHandlerTests.cs
@@ -1,10 +1,14 @@
 using ErrorOr;
 using FluentAssertions;
 using FluentAssertions.CSharpFunctionalExtensions;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 using TrafficSimulator.Application.Commons.Interfaces;
 using TrafficSimulator.Application.Handlers;
 using TrafficSimulator.Domain.Commons;
 using TrafficSimulator.Domain.Models;
+using TrafficSimulator.Infrastructure.CarGenerators.Repositories;
 using TrafficSimulator.Infrastructure.Cars;
 using TrafficSimulator.Infrastructure.Intersections;
 using Xunit.Abstractions;
@@ -13,11 +17,23 @@ namespace TrafficSimulator.Application.UnitTests.SimulationHandlerTests
 {
 	public class SimulationHandlerTests
 	{
-		private readonly ITestOutputHelper _testOutputHelper;
+		private readonly ILogger<SimulationHandlerTests> _logger;
+		private readonly ILoggerFactory _loggerFactory;
 
 		public SimulationHandlerTests(ITestOutputHelper testOutputHelper)
 		{
-			_testOutputHelper = testOutputHelper;
+			var logger = new LoggerConfiguration()
+				.MinimumLevel.Verbose()
+				.WriteTo.TestOutput(testOutputHelper, LogEventLevel.Verbose)
+				.CreateLogger();
+
+			// Create ILoggerFactory using Serilog
+			_loggerFactory = LoggerFactory.Create(builder =>
+			{
+				builder.AddSerilog(logger, dispose: true);
+			});
+
+			_logger = _loggerFactory.CreateLogger<SimulationHandlerTests>();
 		}
 
 		[Fact]
@@ -62,11 +78,11 @@ namespace TrafficSimulator.Application.UnitTests.SimulationHandlerTests
 			};
 
 			IIntersectionRepository intersectionRepository = new IntersectionManager();
-			ICarGeneratorRepository carGeneratorRepository = new CarGe
+			ICarGeneratorRepository carGeneratorRepository = new CarGeneratorsRepositoryInMemory();
 			ICarRepository carRepository = new CarsRepositoryInMemory();
 
 			ISimulationHandler simulationHandler =
-				new IntersectionSimulationHandler(carRepository);
+				new IntersectionSimulationHandler(carGeneratorRepository, carRepository);
 
 			simulationHandler.LoadIntersection(intersection).IsSuccess.Should().BeTrue();
 
@@ -80,6 +96,7 @@ namespace TrafficSimulator.Application.UnitTests.SimulationHandlerTests
 
 				state.IsError.Should().BeFalse();
 
+				_logger.LogInformation("Next cycle");
 
 			} while (state.Value.SimulationPhase == SimulationPhase.InProgress);
 
